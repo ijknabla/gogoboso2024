f7ea6dec4fd3a694c05d5260489d7bb5e892b011
ijknabla <ijknabla@gmail.com>
2024-09-03 11:37:53 +0900

    Access root page

--- bootstrap/__main__.py
+++ bootstrap/__main__.py
@@ -7,6 +7,8 @@ from typing import Protocol, TypeVar
 import click
 import pyppeteer
 
+ROOT_URI = "https://platinumaps.jp/d/gogoboso2024"
+
 
 @click.command()
 @click.option("--headless/--headful", default=True)
@@ -15,7 +17,12 @@ async def main(
     *,
     headless: bool,
 ) -> None:
-    async with _closing(await pyppeteer.launch(headless=headless)):
+    async with _closing(
+        await pyppeteer.launch(args=["--lang=ja"], headless=headless)
+    ) as browser:
+        (page,) = await browser.pages()
+        await page.setExtraHTTPHeaders({"Accept-Language": "ja-JP"})
+        await page.goto(ROOT_URI)
         input("Press Enter!")
 
 
