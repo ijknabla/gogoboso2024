67494b8044920be4b3c751aaed0772406d71c490
ijknabla <ijknabla@gmail.com>
2024-09-03 11:19:56 +0900

    Add browser

--- bootstrap/__main__.py
+++ bootstrap/__main__.py
@@ -2,12 +2,18 @@ from asyncio import run
 from functools import wraps
 
 import click
+import pyppeteer
 
 
 @click.command()
+@click.option("--headless/--headful", default=True)
 @(lambda f: wraps(f)(lambda *args, **kwargs: run(f(*args, **kwargs))))
-async def main() -> None:
-    pass
+async def main(
+    *,
+    headless: bool,
+) -> None:
+    browser = await pyppeteer.launch(headless=headless)
+    await browser.close()
 
 
 if __name__ == "__main__":
