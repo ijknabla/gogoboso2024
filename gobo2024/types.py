14d7cd195ef94817b63935659bfc8a5f29561b2e
ijknabla <ijknabla@gmail.com>
2024-09-04 02:51:53 +0900

    Add BootOptions model

---
+++ gobo2024/types.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel, ConfigDict, StrictInt
+
+
+class StampRallySpot(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
+    # isPublicSpot=True
+    # useCheckinSurvey=False
+    # isExternalCheckin=False
+    # isRewardRequiredSpot=False
+    # isCheckinDisabled=False
+    # hasSpotReward=False
+    # useGps=False
+    # maxCheckinCountForUser=0
+    # quizTryAnswerLimit=0
+    # keywordLength=0
+    # checkinPoints=3
+    # checkinKey=0
+    # sortOrder=0
+    # gpsAcceptableRange=0
+    id: StrictInt  # =58023
+    # stampRallyId=852
+    # spotLng=139.982586996195
+    # spotLat=35.6965389476619
+    # spotId=337568
+    # spotRewards=None
+    # survey=None
+    # quizSelectorTexts=[]
+    # spotGroupIds=[]
+    # stampType='QRCode'
+    # spotRewardType='Application'
+    # pointCurrencyType='Unset'
+    # spotShareType='Public'
+    # stampTypeText='QRCode'
+    # pointCurrencyTypeText='Unset'
+    # spotShareTypeText='Public'
+    # samplePhotoDescription=None
+    # samplePhotoUrl=None
+    # spotTitle='街のピザ屋\u3000コンパーレ\u3000コマーレ'
+    # keywordImageUrl=None
+    # keywordDescription=None
+    # keywordTitle=None
+    # spotRewardSubType='Normal'
+    # spotRewardDescription=None
+    # spotRewardTitle=None
+    # spotRewardBannerUrl=None
+    # stampRallyBaseIcon=None
+    # stampRallyIcon='https://platinumaps.blob.core.windows.net/maps/857/spots/337568/stamprally/852.webp?v=638604079352646211'
+
+
+class BootOptions(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
+    stampRallySpots: list[StampRallySpot]
