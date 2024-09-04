d7ae171032d7545e2c0821fe21d659a5296273c3
ijknabla <ijknabla@gmail.com>
2024-09-04 16:12:13 +0900

    Add csv output

--- gobo2024/__main__.py
+++ gobo2024/__main__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import csv
 import sys
 from asyncio import run
 from functools import wraps
@@ -30,6 +31,25 @@ async def boot_options(
     async with open_page(headless=headless) as page:
         boot_options = await scrape_boot_options(page)
         print(boot_options.model_dump_json(indent=indent), file=output)
+
+
+@main.command
+@click.option(
+    "-o", "--output", type=click.File("w", encoding="utf-8-sig"), default=sys.stdout
+)
+@(lambda f: wraps(f)(lambda *args, **kwargs: run(f(*args, **kwargs))))
+async def googlemap(
+    *,
+    output: IO[str],
+) -> None:
+    async with open_page(headless=True) as page:
+        boot_options = await scrape_boot_options(page)
+
+    writer = csv.writer(output)
+
+    writer.writerow(["スポット", "種類", "経度", "緯度"])
+    for x in sorted(boot_options.stampRallySpots, key=lambda spot: spot.spotId):
+        writer.writerow([x.spotTitle, x.stampType, x.spotLng, x.spotLat])
 
 
 if __name__ == "__main__":
