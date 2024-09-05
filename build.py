fefec8dab10e45227498d8553cd772e740ee8274
ijknabla <ijknabla@gmail.com>
2024-09-05 18:36:38 +0900

    Add StampType

--- build.py
+++ build.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from typing import cast
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from bootstrap import load_boot_options
-from gobo2024.db import SpotLocation, SpotTitle, Table
+from gobo2024 import db
+from gobo2024.types import StampId, StampType
 
 
 def main() -> None:
@@ -12,22 +14,34 @@ def main() -> None:
     engine = create_engine(f"sqlite:///{path}")
 
     # CREATE TABLE
-    Table.metadata.create_all(engine)
+    db.Table.metadata.create_all(engine)
 
     # load object
     boot_options = load_boot_options()
 
+    stamp_id = {
+        cast(StampType, k): StampId(v) for v, k in enumerate(["GPS", "QRCode"], start=1)
+    }
+
     with Session(engine) as session:
+        for _text, _id in stamp_id.items():
+            session.add(
+                db.StampType(
+                    id=_id,
+                    text=_text,
+                )
+            )
+
         for x in sorted(boot_options.stampRallySpots, key=lambda x: x.spotId):
             session.add(
-                SpotTitle(
+                db.SpotTitle(
                     id=x.spotId,
                     text=x.spotTitle,
                 )
             )
 
             session.add(
-                SpotLocation(
+                db.SpotLocation(
                     id=x.spotId,
                     longitude=x.spotLng,
                     latitude=x.spotLat,
