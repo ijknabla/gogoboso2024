75f72e8347a672074d04e9d24015c13ace432e87
ijknabla <ijknabla@gmail.com>
2024-09-05 14:38:45 +0900

    Implement load_boot_uptions

---
+++ bootstrap/__init__.py
@@ -0,0 +1,19 @@
+import json
+from contextlib import ExitStack
+from functools import cache
+from importlib.resources import as_file, files
+
+from gobo2024.types import BootOptions
+
+
+@cache
+def load_boot_options() -> BootOptions:
+    with ExitStack() as stack:
+        enter = stack.enter_context
+
+        boot_options_path = enter(
+            as_file(files(__package__).joinpath("resources/bootOptions.json"))
+        )
+        boot_options_io = enter(boot_options_path.open("r", encoding="utf-8"))
+
+        return BootOptions.model_validate(json.load(boot_options_io))
