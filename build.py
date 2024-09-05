c49a438e10f77358255772e19bdf813387f5c52a
ijknabla <ijknabla@gmail.com>
2024-09-05 15:05:26 +0900

    Add CREATE TABLE in build.py

--- build.py
+++ build.py
@@ -1,12 +1,16 @@
-import sqlite3
 from pathlib import Path
+
+from sqlalchemy import create_engine
+
+from gobo2024.db import Table
 
 
 def main() -> None:
-    db = (Path(__file__) / "../gobo2024/gobo2024.db").resolve()
+    path = (Path(__file__) / "../gobo2024/gobo2024.db").resolve()
+    engine = create_engine(f"sqlite:///{path}")
 
-    connection = sqlite3.connect(db)
-    connection.close()
+    # CREATE TABLE
+    Table.metadata.create_all(engine)
 
 
 if __name__ == "__main__":
