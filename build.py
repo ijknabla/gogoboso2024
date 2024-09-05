ceae577288ddf7343217c5e48715535c888b62ff
ijknabla <ijknabla@gmail.com>
2024-09-05 13:51:08 +0900

    Add db

--- build.py
+++ build.py
@@ -1,5 +1,12 @@
+import sqlite3
+from pathlib import Path
+
+
 def main() -> None:
-    pass
+    db = (Path(__file__) / "../gobo2024/gobo2024.db").resolve()
+
+    connection = sqlite3.connect(db)
+    connection.close()
 
 
 if __name__ == "__main__":
