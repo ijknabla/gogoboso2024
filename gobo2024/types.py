ca1fa40aca591a94e62eac1e1ed1e6c48e71ecac
ijknabla <ijknabla@gmail.com>
2024-09-06 11:54:05 +0900

    Put default values

--- gobo2024/types.py
+++ gobo2024/types.py
@@ -3,7 +3,6 @@ from typing import Annotated, Literal, NewType
 from pydantic import (
     BaseModel,
     ConfigDict,
-    StrictBool,
     StrictFloat,
     StrictInt,
     StrictStr,
@@ -27,27 +26,27 @@ SpotRewardSubType = Literal["Normal"]
 class StampRallySpot(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
-    isPublicSpot: StrictBool  # =True
-    useCheckinSurvey: StrictBool  # =False
-    isExternalCheckin: StrictBool  # =False
-    isRewardRequiredSpot: StrictBool  # =False
-    isCheckinDisabled: StrictBool  # =False
-    hasSpotReward: StrictBool  # =False
-    useGps: Literal[False]  # =False
-    maxCheckinCountForUser: StrictInt  # =0
-    quizTryAnswerLimit: StrictInt  # =0
-    keywordLength: StrictInt  # =0
+    isPublicSpot: Literal[True] = True
+    useCheckinSurvey: Literal[False] = False
+    isExternalCheckin: Literal[False] = False
+    isRewardRequiredSpot: Literal[False] = False
+    isCheckinDisabled: Literal[False] = False
+    hasSpotReward: Literal[False] = False
+    useGps: Literal[False] = False
+    maxCheckinCountForUser: Literal[0] = 0
+    quizTryAnswerLimit: Literal[0] = 0
+    keywordLength: Literal[0] = 0
     checkinPoints: StrictInt  # =3
-    checkinKey: StrictInt  # =0
-    sortOrder: StrictInt  # =0
-    gpsAcceptableRange: StrictInt  # =0
+    checkinKey: Literal[0] = 0
+    sortOrder: Literal[0] = 0
+    gpsAcceptableRange: Literal[0] = 0
     id: StrictInt  # =58023
-    stampRallyId: Literal[852]  # =852
+    stampRallyId: Literal[852] = 852
     spotLng: Annotated[Longitude, StrictFloat]  # =139.982586996195
     spotLat: Annotated[Latitude, StrictFloat]  # =35.6965389476619
     spotId: Annotated[SpotId, StrictInt]  # =337568
-    spotRewards: None  # =None
-    survey: None  # =None
+    spotRewards: None = None
+    survey: None = None
     quizSelectorTexts: list[StrictStr]  # =[]
     spotGroupIds: list[StrictInt]  # =[]
     stampType: StampType  # ='QRCode'
@@ -57,17 +56,17 @@ class StampRallySpot(BaseModel):
     stampTypeText: StampTypeText  # ='QRCode'
     pointCurrencyTypeText: PointCurrencyTypeText  # ='Unset'
     spotShareTypeText: SpotShareTypeText  # ='Public'
-    samplePhotoDescription: None  # =None
-    samplePhotoUrl: None  # =None
+    samplePhotoDescription: None = None
+    samplePhotoUrl: None = None
     spotTitle: StrictStr  # ='街のピザ屋\u3000コンパーレ\u3000コマーレ'
-    keywordImageUrl: None  # =None
-    keywordDescription: None  # =None
-    keywordTitle: None  # =None
+    keywordImageUrl: None = None
+    keywordDescription: None = None
+    keywordTitle: None = None
     spotRewardSubType: SpotRewardSubType  # ='Normal'
-    spotRewardDescription: None  # =None
-    spotRewardTitle: None  # =None
-    spotRewardBannerUrl: None  # =None
-    stampRallyBaseIcon: None  # =None
+    spotRewardDescription: None = None
+    spotRewardTitle: None = None
+    spotRewardBannerUrl: None = None
+    stampRallyBaseIcon: None = None
     stampRallyIcon: StrictStr  # ='https://platinumaps.blob.core.windows.net/maps/857/spots/337568/stamprally/852.webp?v=638604079352646211'
 
 
