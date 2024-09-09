450c91426dac29dedf5c8b1357dc17cfadfb1281
ijknabla <ijknabla@gmail.com>
2024-09-09 12:20:55 +0900

    Update formatter

--- script/__main__.py
+++ script/__main__.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 from pathlib import Path
 
 import click
-from polars import DataType, Datetime, String, UInt32, col, read_csv
+from polars import DataFrame, DataType, Datetime, String, UInt32, col, read_csv
+from sqlalchemy.orm import Session
+
+from gobo2024 import db
 
 
 @click.command()
@@ -18,11 +21,54 @@ def main(log_csv: Path | None = None) -> None:
         "title": String,
     }
 
-    log = read_csv(log_csv, schema=dtypes).with_columns(
-        col("time").dt.convert_time_zone("Asia/Tokyo"),
+    log = (
+        read_csv(log_csv, schema=dtypes)
+        .with_columns(
+            col("time").dt.convert_time_zone("Asia/Tokyo"),
+        )
+        .with_row_index(name="row", offset=2)
     )
 
-    log.write_csv(log_csv, include_bom=True)
+    with Session(db.create_engine()) as s:
+        spot_titles = []
+
+        for row, id_in_csv, title_in_csv in log.select(
+            "row", "id", "title"
+        ).iter_rows():
+            if (
+                by_id := s.query(db.SpotTitle)
+                .filter(db.SpotTitle.id == id_in_csv)
+                .all()
+            ):
+                (spot_title,) = by_id
+            elif (
+                by_title := s.query(db.SpotTitle)
+                .filter(db.SpotTitle.text.contains(title_in_csv))
+                .all()
+            ):
+                spot_title, *rest = by_title
+                if rest:
+                    candidates = sorted(x.text for x in by_title)
+                    message = (
+                        f"{row=!r}: "
+                        f"Ambiguous spot title {title_in_csv!r} in {candidates!r}"
+                    )
+                    raise ValueError(message)
+            else:
+                message = f"{row=!r}: {id_in_csv=!r}, {title_in_csv=!r}"
+                raise ValueError(message)
+
+            spot_titles.append(spot_title)
+
+        log = log.update(
+            DataFrame(
+                {
+                    "id": [x.id for x in spot_titles],
+                    "title": [x.text for x in spot_titles],
+                }
+            )
+        )
+    log.select(*dtypes).write_csv(log_csv, include_bom=True)
 
 
 if __name__ == "__main__":
