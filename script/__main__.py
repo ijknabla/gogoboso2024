483d652185d712e93a2a664c7e8f4ad8eeb626e9
ijknabla <ijknabla@gmail.com>
2024-09-09 11:57:31 +0900

    Update by dtypes

--- script/__main__.py
+++ script/__main__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from pathlib import Path
 
 import click
-from polars import Datetime, String, UInt32, col, read_csv
+from polars import DataType, Datetime, String, UInt32, col, read_csv
 
 
 @click.command()
@@ -12,10 +12,14 @@ def main(log_csv: Path | None = None) -> None:
     if log_csv is None:
         log_csv = Path(__file__, "../log.csv").resolve()
 
-    log = read_csv(log_csv).with_columns(
-        col("id").cast(UInt32),
-        col("time").cast(Datetime(time_unit="ms")).dt.replace_time_zone("Asia/Tokyo"),
-        col("title").cast(String),
+    dtypes: dict[str, DataType | type[DataType]] = {
+        "id": UInt32,
+        "time": Datetime(time_unit="ms"),
+        "title": String,
+    }
+
+    log = read_csv(log_csv, schema=dtypes).with_columns(
+        col("time").dt.convert_time_zone("Asia/Tokyo"),
     )
 
     log.write_csv(log_csv, include_bom=True)
