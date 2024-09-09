2d36c8a08d4c753b05b32b5bfe728ec1d82c4339
ijknabla <ijknabla@gmail.com>
2024-09-09 13:27:20 +0900

    Update output

--- script/__main__.py
+++ script/__main__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import csv
 from pathlib import Path
-from typing import IO
+from typing import IO, Any
 
 import click
 from polars import DataFrame, DataType, Datetime, String, UInt32, col, read_csv
@@ -14,9 +14,11 @@ from gobo2024 import db
 @click.command()
 @click.option("--log-csv", type=click.Path(exists=True, dir_okay=False, path_type=Path))
 @click.option("--togo", type=click.File(mode="w", encoding="utf-8-sig"))
+@click.option("--done", type=click.File(mode="w", encoding="utf-8-sig"))
 def main(
     log_csv: Path | None = None,
     togo: IO[str] | None = None,
+    done: IO[str] | None = None,
 ) -> None:
     if log_csv is None:
         log_csv = Path(__file__, "../log.csv").resolve()
@@ -80,37 +82,69 @@ def main(
     log.select(*dtypes).write_csv(log_csv, include_bom=True)
 
     if togo:
-        acquired = set(log["id"])
+        with Session(engine) as session:
+            _write_togo(csv.writer(togo), log, session)
 
-        writer = csv.writer(togo)
-        writer.writerow(["名前", "種類", "経度", "緯度"])
+    if done:
+        with Session(engine) as session:
+            _write_done(csv.writer(done), log, session)
 
-        with Session(engine) as s:
-            for title in (
-                s.query(db.SpotTitle).filter(db.SpotTitle.id.not_in(acquired)).all()
-            ):
-                stamp_id = (
-                    s.query(db.SpotStamp.stamp_id)
-                    .filter(db.SpotStamp.id == title.id)
-                    .scalar_subquery()
-                )
-                stamp_type = (
-                    s.query(db.StampType).filter(db.StampType.id == stamp_id).first()
-                )
-                if stamp_type is None:
-                    raise RuntimeError
 
-                location = (
-                    s.query(db.SpotLocation)
-                    .filter(db.SpotLocation.id == title.id)
-                    .first()
-                )
-                if location is None:
-                    raise RuntimeError
+def _write_togo(writer: Any, log: DataFrame, session: Session) -> None:
+    writer.writerow(["名前", "種類", "経度", "緯度"])
 
-                writer.writerow(
-                    [title.text, stamp_type.text, location.longitude, location.latitude]
-                )
+    acquired = set(log["id"])
+    for title in (
+        session.query(db.SpotTitle).filter(db.SpotTitle.id.not_in(acquired)).all()
+    ):
+        stamp_id = (
+            session.query(db.SpotStamp.stamp_id)
+            .filter(db.SpotStamp.id == title.id)
+            .scalar_subquery()
+        )
+        stamp_type = (
+            session.query(db.StampType).filter(db.StampType.id == stamp_id).first()
+        )
+        if stamp_type is None:
+            raise RuntimeError
+
+        location = (
+            session.query(db.SpotLocation)
+            .filter(db.SpotLocation.id == title.id)
+            .first()
+        )
+        if location is None:
+            raise RuntimeError
+
+        writer.writerow(
+            [title.text, stamp_type.text, location.longitude, location.latitude]
+        )
+
+
+def _write_done(writer: Any, log: DataFrame, session: Session) -> None:
+    writer.writerow(["通し番号", "時刻", "名前", "経度", "緯度"])
+
+    for index, (id_, time, title) in enumerate(
+        log.sort(["time", "row"], descending=[False, False], maintain_order=True)
+        .select("id", "time", "title")
+        .iter_rows(),
+        start=1,
+    ):
+        location = (
+            session.query(db.SpotLocation).filter(db.SpotLocation.id == id_).first()
+        )
+        if location is None:
+            raise RuntimeError
+
+        writer.writerow(
+            [
+                f"{index:0>3}",
+                time.strftime("%Y年%m月%d日%H時%M分"),
+                title,
+                location.longitude,
+                location.latitude,
+            ]
+        )
 
 
 if __name__ == "__main__":
