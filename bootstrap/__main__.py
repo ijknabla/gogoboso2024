eba3b21d685ced32d61c738da18be1e49b3ddf62
ijknabla <ijknabla@gmail.com>
2024-09-10 15:02:03 +0900

    Implement spot_detail.json output

--- bootstrap/__main__.py
+++ bootstrap/__main__.py
@@ -1,10 +1,22 @@
-from asyncio import run
+from __future__ import annotations
+
+from asyncio import CancelledError, create_task, gather, run, wait
+from asyncio.queues import Queue
+from collections import ChainMap
+from contextlib import suppress
 from functools import wraps
 from importlib.resources import as_file, files
+from typing import TYPE_CHECKING
 
 import click
+from pydantic import RootModel
 
-from .scraping import open_new_page, scrape_boot_options
+from gobo2024.types import SpotDetail, SpotId
+
+from .scraping import open_new_page, scrape_boot_options, scrape_spot_detail
+
+if TYPE_CHECKING:
+    from pyppeteer.page import Page
 
 
 @click.group()
@@ -14,16 +26,42 @@ def main() -> None:
 
 @main.command()
 @click.option("--headless/--headful", default=True)
+@click.option("-n", type=int, default=0)
 @click.option("--indent", type=int, default=1)
 @click.option(
     "--with-event-hub-context/--without-event-hub-context",
     default=False,
 )
 @(lambda f: wraps(f)(lambda *args, **kwargs: run(f(*args, **kwargs))))
-async def update(*, headless: bool, indent: int, with_event_hub_context: bool) -> None:
+async def update(
+    *, headless: bool, n: int, indent: int, with_event_hub_context: bool
+) -> None:
     async with open_new_page(headless=headless) as new_page:
         page = await new_page()
         boot_options = await scrape_boot_options(page)
+
+        spots = Queue[SpotId]()
+        for x in boot_options.stampRallySpots:
+            await spots.put(x.spotId)
+
+        pages = [page]
+        for _ in range(n - 1):
+            pages.append(await new_page())  # noqa: PERF401
+
+        spot_detail = RootModel[dict[SpotId, SpotDetail]](
+            dict(
+                sorted(
+                    ChainMap(
+                        *await gather(
+                            *(
+                                _scrape_spot_detail(page, spots)
+                                for rank, page in enumerate(pages)
+                            )
+                        )
+                    ).items()
+                )
+            )
+        )
 
     exclude = set[str]()
     if not with_event_hub_context:
@@ -34,6 +72,39 @@ async def update(*, headless: bool, indent: int, with_event_hub_context: bool) -
             boot_options.model_dump_json(indent=indent, exclude=exclude) + "\n",
             encoding="utf-8",
         )
+
+        (resources / "spot_detail.json").write_text(
+            spot_detail.model_dump_json(indent=indent) + "\n", encoding="utf-8"
+        )
+
+
+async def _scrape_spot_detail(
+    page: Page, queue: Queue[SpotId]
+) -> dict[SpotId, SpotDetail]:
+    result = dict[SpotId, SpotDetail]()
+
+    queue_join = create_task(queue.join())
+
+    while True:
+        queue_get = create_task(queue.get())
+
+        done, pending = await wait(
+            {queue_join, queue_get}, return_when="FIRST_COMPLETED"
+        )
+
+        if queue_get in done:
+            queue.task_done()
+            spot_id = queue_get.result()
+            result[spot_id] = await scrape_spot_detail(page, spot_id=spot_id)
+        else:
+            for task in pending:
+                task.cancel()
+            for task in pending:
+                with suppress(CancelledError):
+                    await task
+            break
+
+    return result
 
 
 if __name__ == "__main__":
