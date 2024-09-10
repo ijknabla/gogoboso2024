7aa9647c4c40ff2a84398275585ca0f48aead735
ijknabla <ijknabla@gmail.com>
2024-09-10 14:27:42 +0900

    Add scrape_spot_detail

--- bootstrap/scraping.py
+++ bootstrap/scraping.py
@@ -7,12 +7,15 @@ from typing import TYPE_CHECKING
 
 from pyppeteer import launch
 
-from gobo2024.types import BootOptions
+from gobo2024.types import Address, BootOptions, SpotDetail, SpotId
 
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator, Awaitable, Callable
 
     from pyppeteer.page import Page
+
+
+URI = "https://platinumaps.jp/d/gogoboso2024"
 
 
 @asynccontextmanager
@@ -41,25 +44,14 @@ async def open_new_page(
 async def scrape_boot_options(
     page: Page,
     *,
-    uri: str = "https://platinumaps.jp/d/gogoboso2024",
+    uri: str = URI,
     target: str = "window.__bootOptions",
 ) -> BootOptions:
     return await _find_boot_options(
         page,
-        uri=await _find_map_uri(page, uri=uri),
+        uri=await _find_iframe_src(page, uri=uri),
         target=target,
     )
-
-
-async def _find_map_uri(page: Page, uri: str) -> str:
-    xpath = "//iframe"
-    await page.goto(uri)
-    await page.waitForXPath(xpath)
-    for element in await page.Jx(xpath):
-        map_uri: str = await (await element.getProperty("src")).jsonValue()
-        return map_uri
-
-    raise RuntimeError
 
 
 async def _find_boot_options(page: Page, uri: str, target: str) -> BootOptions:
@@ -75,4 +67,49 @@ async def _find_boot_options(page: Page, uri: str, target: str) -> BootOptions:
 
         return BootOptions.model_validate_json(matched.group("json"))
 
+    raise RuntimeError
+
+
+async def scrape_spot_detail(
+    page: Page, spot_id: SpotId, *, uri: str = URI
+) -> SpotDetail:
+    return await _find_spot_detail(
+        page, uri=await _find_iframe_src(page, uri + f"?spot={spot_id}")
+    )
+
+
+async def _find_spot_detail(page: Page, uri: str) -> SpotDetail:
+    await page.goto(uri)
+
+    title, subtitle, description, address = await gather(
+        _find_text_by_xpath(page, '//div[@class="detail__title"]'),
+        _find_text_by_xpath(page, '//div[@class="detail__subtitletext"]'),
+        _find_text_by_xpath(page, '//div[@class="ptmdescription__text"]'),
+        _find_text_by_xpath(page, "//address"),
+    )
+
+    return SpotDetail(
+        title=title,
+        subtitle=subtitle,
+        description=description,
+        address=Address(address),
+    )
+
+
+async def _find_iframe_src(page: Page, uri: str) -> str:
+    xpath = "//iframe"
+    await page.goto(uri)
+    await page.waitForXPath(xpath)
+    for element in await page.Jx(xpath):
+        map_uri: str = await (await element.getProperty("src")).jsonValue()
+        return map_uri
+
+    raise RuntimeError
+
+
+async def _find_text_by_xpath(page: Page, xpath: str) -> str:
+    await page.waitForXPath(xpath)
+    for element in await page.Jx(xpath):
+        text: str = await (await element.getProperty("textContent")).jsonValue()
+        return text
     raise RuntimeError
